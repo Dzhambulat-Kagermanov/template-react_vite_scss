ca21d610f451a75654131013f323087737490065
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-11-16 09:31:32 +0300

    INITIALIZATION

---
+++ src/shared/types/shared.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export type TClassName = { className?: string }
+export type TWrapperClassName = { wrapperClassName?: string }
+export type TModuleClassName = TClassName & TWrapperClassName
+export type TChildren = { children: ReactNode }
+export interface TTag {
+	tag?: keyof JSX.IntrinsicElements
+}
